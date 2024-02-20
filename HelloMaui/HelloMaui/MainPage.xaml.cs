83ee4d22e000b9d3504715d7bd2c410303b8a8c3
Thomas Galliker <thomas@bluewin.ch>
2024-02-20 12:31:19 +0100

    Adjust sample app to be in sync with slides

--- HelloMaui/HelloMaui/MainPage.xaml.cs
+++ HelloMaui/HelloMaui/MainPage.xaml.cs
@@ -2,24 +2,17 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private int count = 0;
 
         public MainPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Button_OnClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            this.count++;
+            this.MyButton.Text = $"Clicked {this.count}";
         }
     }
-
 }
