ea60ec39fbff8513a675edd4a47b1cf2fcd0fca1
Thomas Galliker <thomas@bluewin.ch>
2024-02-20 10:07:58 +0100

    Implement button click handler on iOS app

--- MobileApp/iOSApp/AppDelegate.cs
+++ MobileApp/iOSApp/AppDelegate.cs
@@ -1,9 +1,15 @@
+
+using System.Drawing;
+
 namespace iOSApp
 {
     [Register("AppDelegate")]
     public class AppDelegate : UIApplicationDelegate
     {
-        public override UIWindow? Window
+        private UIButton myButton;
+        private int counter = 0;
+
+        public override UIWindow Window
         {
             get;
             set;
@@ -13,22 +19,33 @@ namespace iOSApp
         {
             // create a new window instance based on the screen size
             this.Window = new UIWindow(UIScreen.MainScreen.Bounds);
+            this.Window.BackgroundColor = UIColor.White;
 
-            // create a UIViewController with a single UILabel
+            // create a UIViewController with a single UIButton
             var vc = new UIViewController();
-            vc.View!.AddSubview(new UILabel(this.Window!.Frame)
+
+            this.myButton = new UIButton(new RectangleF(0, 0, 300, 30))
             {
-                BackgroundColor = UIColor.SystemBackground,
-                TextAlignment = UITextAlignment.Center,
-                Text = "Hello, iOS!",
-                AutoresizingMask = UIViewAutoresizing.All,
-            });
+                Center = this.Window.Center,
+            };
+            this.myButton.SetTitle("Click here!", UIControlState.Normal);
+            this.myButton.SetTitleColor(UIColor.SystemBlue, UIControlState.Normal);
+            this.myButton.TouchUpInside += this.MyButtonOnTouchUpInside;
+
+            vc.View.AddSubview(this.myButton);
+
             this.Window.RootViewController = vc;
 
             // make the window visible
             this.Window.MakeKeyAndVisible();
 
             return true;
+        }
+
+        private void MyButtonOnTouchUpInside(object sender, EventArgs e)
+        {
+            this.counter++;
+            this.myButton.SetTitle($"Clicked {this.counter}", UIControlState.Normal);
         }
     }
 }
