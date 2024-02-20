a3ad459de00d717e2a59f57b6f1d0beff6a44f3f
Thomas Galliker <thomas@bluewin.ch>
2024-02-20 17:09:36 +0100

    Add constants

---
+++ XamlDemo/XamlDemo/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace XamlDemo
+{
+    internal static class Constants
+    {
+        public const string ConstantString = "This is a const string";
+        public static readonly string StaticReadonlyString = "This is a static readonly string";
+    }
+}
