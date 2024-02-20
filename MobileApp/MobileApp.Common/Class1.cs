0af5af9b51c258a57bc347f37c345a873d77841a
Thomas Galliker <thomas@bluewin.ch>
2024-02-20 08:38:49 +0100

    Add code sharing projects

---
+++ MobileApp/MobileApp.Common/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileApp.Common
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+        }
+    }
+}
---
+++ MobileApp/MobileApp.Shared/Class2.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileApp.Shared
+{
+    public class Class2
+    {
+        public void Method2()
+        {
+        }
+    }
+}
