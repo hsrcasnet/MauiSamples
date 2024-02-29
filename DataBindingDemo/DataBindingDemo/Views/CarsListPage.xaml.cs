22a75e0112295a10938fdfa6596ece5a73af3331
Thomas Galliker <thomas@bluewin.ch>
2024-02-29 18:12:25 +0100

    Add collection binding demos

--- DataBindingDemo/DataBindingDemo/Views/CarsListPage.xaml.cs
+++ DataBindingDemo/DataBindingDemo/Views/CarsListPage.xaml.cs
@@ -1,3 +1,5 @@
+using DataBindingDemo.Model;
+
 namespace DataBindingDemo.Views;
 
 public partial class CarsListPage : ContentPage
@@ -5,5 +7,16 @@ public partial class CarsListPage : ContentPage
     public CarsListPage()
     {
         this.InitializeComponent();
+    }
+
+    private async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        if (e.Item is Car car)
+        {
+            await this.DisplayAlert("ListView.ItemTapped", $"{car.Brand} {car.Model} was tapped.", "OK");
+
+            // Deselect item
+            ((ListView)sender).SelectedItem = null;
+        }
     }
 }
