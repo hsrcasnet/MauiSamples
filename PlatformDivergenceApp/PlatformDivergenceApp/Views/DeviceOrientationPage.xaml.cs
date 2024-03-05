12374b6328bc2025ebdba4fb97aa05e81d4b55f3
Thomas Galliker <thomas@bluewin.ch>
2024-03-05 17:46:13 +0100

    Add demo for OnPlatform

--- PlatformDivergenceApp/PlatformDivergenceApp/Views/DeviceOrientationPage.xaml.cs
+++ PlatformDivergenceApp/PlatformDivergenceApp/Views/DeviceOrientationPage.xaml.cs
@@ -1,3 +1,4 @@
+using PlatformDivergenceApp.Services.Orientation;
 using PlatformDivergenceApp.ViewModels;
 
 namespace PlatformDivergenceApp.Views;
@@ -7,6 +8,10 @@ public partial class DeviceOrientationPage : ContentPage
     public DeviceOrientationPage(DeviceOrientationViewModel deviceOrientationViewModel)
     {
         this.InitializeComponent();
+
+        // Demo: DeviceInfoService can be create in platform-independent code or injected via DI.
+        //IDeviceOrientationService deviceOrientationService = new DeviceOrientationService();
+        //var deviceOrientationViewModel = new DeviceOrientationViewModel(deviceOrientationService);
         this.BindingContext = deviceOrientationViewModel;
     }
 }
