5bbb2a53a6682a8ce2c55b02a412caccc27bd0bf
Thomas Galliker <thomas@bluewin.ch>
2024-03-06 15:09:47 +0100

    Add demo app for Prism.Maui

---
+++ PrismMauiApp/PrismMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PrismMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
