5bb7952193ce85c82978bb55dc275c3f90c20858
Thomas Galliker <>
2024-08-16 08:41:10 +0200

    Add sample for modal navigation. Add demo for OnBackButtonPressed.

--- NavigationDemo/NavigationDemo/Views/DetailPage1.xaml.cs
+++ NavigationDemo/NavigationDemo/Views/DetailPage1.xaml.cs
@@ -1,28 +1,47 @@
 using System.Diagnostics;
 
-namespace NavigationDemo.Views;
-
-public partial class DetailPage1 : ContentPage
+namespace NavigationDemo.Views
 {
-	public DetailPage1()
-	{
-        this.InitializeComponent();
-	}
-
-    private async void NavigateToDetailPage2Async(object sender, EventArgs e)
+    public partial class DetailPage1 : ContentPage
     {
-        await this.Navigation.PushAsync(new DetailPage2());
-    }
+        public DetailPage1()
+        {
+            this.InitializeComponent();
+        }
 
-    protected override void OnAppearing()
-    {
-        Debug.WriteLine("DetailPage1: OnAppearing");
-        base.OnAppearing();
-    }
+        private async void NavigateToDetailPage2Async(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new DetailPage2());
+        }
 
-    protected override void OnDisappearing()
-    {
-        Debug.WriteLine("DetailPage1: OnDisappearing");
-        base.OnDisappearing();
+        protected override void OnAppearing()
+        {
+            Debug.WriteLine("DetailPage1: OnAppearing");
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            Debug.WriteLine("DetailPage1: OnDisappearing");
+            base.OnDisappearing();
+        }
+
+        protected override void OnNavigatedTo(NavigatedToEventArgs args)
+        {
+            Debug.WriteLine("DetailPage1: OnNavigatedTo");
+            base.OnNavigatedTo(args);
+        }
+
+        protected override void OnNavigatingFrom(NavigatingFromEventArgs args)
+        {
+            Debug.WriteLine("DetailPage1: OnNavigatingFrom");
+            base.OnNavigatingFrom(args);
+        }
+
+        protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
+        {
+            Debug.WriteLine("DetailPage1: OnNavigatedFrom");
+            base.OnNavigatedFrom(args);
+        }
     }
 }
